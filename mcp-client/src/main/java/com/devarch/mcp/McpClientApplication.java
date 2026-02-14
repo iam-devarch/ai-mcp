c601f20f183fe859370cfc0aca4397e35b06b394
Dev Arch <iam.devarch@gmail.com>
2026-02-15 02:01:37 +0800

    Enable Mcp Client application to contact using google  (Gemini) model

--- mcp-client/src/main/java/com/devarch/mcp/McpClientApplication.java
+++ mcp-client/src/main/java/com/devarch/mcp/McpClientApplication.java
@@ -1,7 +1,15 @@
 package com.devarch.mcp;
 
+import io.modelcontextprotocol.client.McpSyncClient;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.mcp.SyncMcpToolCallbackProvider;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import org.springframework.ai.chat.model.ChatModel;
+import java.util.List;
 
 @SpringBootApplication
 public class McpClientApplication {
@@ -10,4 +18,22 @@ public class McpClientApplication {
         SpringApplication.run(McpClientApplication.class, args);
     }
 
+    @Bean
+    public ChatClient chatClient(ChatModel chatModel, List<McpSyncClient> mcpClients) {
+        SyncMcpToolCallbackProvider mcpTools = new SyncMcpToolCallbackProvider(mcpClients);
+
+        return ChatClient.builder(chatModel)
+                .defaultToolCallbacks(mcpTools.getToolCallbacks())
+                .build();
+    }
+
+    @Bean
+    public CommandLineRunner runner(ChatClient chatClient) {
+        return args -> {
+            String response = chatClient.prompt("What is the weather in San Francisco?")
+                    .call()
+                    .content();
+            System.out.println("Response: " + response);
+        };
+    }
 }
